f81c611df7f2eba4e665d91a97da963d347e437f
DRAYMOND M H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEDR2@OOCL.COM>
2020-07-22 16:23:26 +0800

    [06][Draymond Xie][Feater] Finish practice06.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    protected  int klass = 0;
+
+    public Teacher(String name, int age) {
+        super(name,age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(String name, int age, int klass) {
+        super(name, age);
+        this.setKlass(klass);
+    }
+
+    public String introduce(){//My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.
+        String classString = "";
+        classString += (this.klass == 0) ? "No Class." : "Class " + this.klass+".";
+        return  super.introduce() + " I am a Teacher. I teach "+ classString;
+    }
 }
