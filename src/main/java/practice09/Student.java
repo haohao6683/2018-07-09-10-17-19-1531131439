d26bc73cddc8a807483b5745f9834b770b34969e
DRAYMOND M H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEDR2@OOCL.COM>
2020-07-22 20:25:19 +0800

    [09][Draymond Xie][Feater] Finish practice09.

--- src/main/java/practice09/Student.java
+++ src/main/java/practice09/Student.java
@@ -1,4 +1,27 @@
 package practice09;
 
-public class Student {
+public class Student extends Person{
+    protected  Klass klass;
+
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Student(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){//>My name is Tom. I am 21 years old. I am a Student. I am Leader of Class 2.
+//        String introduce =  super.introduce() + " I am a Student. I am ";//"at Class "+ this.klass.number +".";
+//        introduce += (name == klass.leader.name) ? "Leader of Class " + klass.number + "." : "at Class "+ klass.number +".";
+        return String.format(
+                super.introduce() +" I am a Student. I am %s Class " + klass.number +".",
+                (klass.leader != null && name == klass.leader.name) ? "Leader of" : "at");
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,38 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    protected  Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id,name,age);
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.setKlass(klass);
+    }
+
+    public String introduce(){//My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.
+//        String classString = "";
+//        classString += (this.klass == null) ? "No Class." : "Class " + this.klass.number+".";
+//        return  super.introduce() + " I am a Teacher. I teach "+ classString;
+        return String.format(super.introduce() + " I am a Teacher. I teach %s.",
+                (this.klass == null) ? "No Class" : "Class " + this.klass.number);
+    }
+
+    public  String introduceWith(Student student){//>My name is Tom. I am 21 years old. I am a Teacher. I teach Jerry.
+//        String teachingStr = "";
+//        teachingStr += (student.klass.number == this.klass.number) ? "teach " + student.name + "." : "don't teach " + student.name + ".";
+//        return  super.introduce() + " I am a Teacher. I "+ teachingStr;
+        return String.format(super.introduce() + " I am a Teacher. I%s teach %s.",
+                (student.klass.number == this.klass.number) ? "" : " don't", student.name);
+    }
 }
