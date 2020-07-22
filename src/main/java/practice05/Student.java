fc574888a87597de48166ca94a47432ed70ecf21
DRAYMOND M H XIE (ITA-ISDC-ISD-OOCLL/ZHA) <XIEDR2@OOCL.COM>
2020-07-22 15:51:12 +0800

    [05] [Draymond Xie] [Feater] Finish practice05

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,22 @@
 package practice05;
 
-public class Student {
+public class Student extends Person{
+    protected  int klass;
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduce(){//My name is Tom. I am 21 years old. I am a Student. I am at Class 2.
+        return  super.introduce() + " I am a Student. I am at Class "+ this.klass +".";
+    }
 }
